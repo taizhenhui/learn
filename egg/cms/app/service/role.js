71c1e4016c8c5d608c87ca07bc6b0528a476b6e5
taizhenhui <52440231@qq.com>
2022-12-02 15:22:44 +0800

    连表查询，查询用户资源

--- egg/cms/app/service/role.js
+++ egg/cms/app/service/role.js
@@ -10,17 +10,10 @@ class Service extends BaseService {
     const { app: { mysql } } = this
     const resources = await mysql.select('resource')
     let rootMenus = []
-    let map = {}
-    resources.forEach(i => {
-      i.children = []
-      map[i.id] = i
-    })
-    resources.forEach(i => {
-      if (i.parent_id === 0) {
-        rootMenus.push(i)
-      } else {
-
-        map[i.parent_id].children.push(i)
+    resources.forEach(item => { // 遍历对象数组
+      item.children = resources.filter(info => info.parent_id === item.id) // 找到每个对象的子节点 
+      if (item.parent_id === 0) {
+        rootMenus.push(item) // 将一层节点放入新数组中
       }
     })
     return rootMenus
@@ -31,7 +24,7 @@ class Service extends BaseService {
     await mysql.query(sql, roleId)
     for (let i = 0, len = resourceIds.length; i < len; i++) {
       let id = resourceIds[i]
-      await mysql.insert('role_resource',{
+      await mysql.insert('role_resource', {
         role_id: roleId,
         resource_id: id
       })
@@ -45,13 +38,22 @@ class Service extends BaseService {
   async setUser({ roleId, userIds }) {
     const { app: { mysql } } = this
     const sql = `delete from role_user where role_id=?`
-    await mysql.query(sql, roleId)
-    for (let i = 0, len = userIds.length; i < len; i++) {
-      let id = userIds[i]
-      await mysql.insert('role_user',{
-        role_id: roleId,
-        user_id: id
-      })
+    const conn = await mysql.beginTransaction()
+    try {
+      await conn.query(sql, roleId)
+      for (let i = 0, len = userIds.length; i < len; i++) {
+        let id = userIds[i]
+        await conn.insert('role_user', {
+          role_id: roleId,
+          user_id: id
+        })
+      }
+      await conn.commit()
+      // (await conn).commit()
+    } catch (err) {
+      // (await conn).rollback
+      await conn.rollback()
+      throw err
     }
   }
 }
--- egg/cms/app/service/user.js
+++ egg/cms/app/service/user.js
@@ -1,10 +1,38 @@
 'use strict';
 
 const BaseService = require('./base')
-class Service extends BaseService{
-  constructor(...args){
+class Service extends BaseService {
+  constructor(...args) {
     super(...args)
     this.entity = 'user'
+  }
+  async list(pageNum, pageSize, where) {
+    // 查询当页的数据 特定页的记录数组
+    const data = await this.app.mysql.select(this.entity, {
+      where,
+      order: [['id', 'asc']],
+      offset: (pageNum - 1) * pageSize,
+      limit: pageSize,
+    })
+    for (let i = 0, len = data.length; i < len; i++) {
+      let user = data[i]
+      const select_sql = `SELECT resource.* FROM resource 
+      INNER JOIN role_resource ON resource.id = role_resource.resource_id
+      INNER JOIN role_user ON role_resource.role_id = role_user.role_id
+      WHERE role_user.user_id=?;`
+      let resources = await this.app.mysql.query(select_sql, user.id)
+      let rootMenus = []
+      resources.forEach(item => { // 遍历对象数组
+        item.children = resources.filter(info => info.parent_id === item.id) // 找到每个对象的子节点 
+        if (item.parent_id === 0) rootMenus.push(item) // 将一层节点放入新数组中
+      })
+      user.resources = rootMenus
+    }
+
+
+    // 总条数
+    const total = await this.app.mysql.count(this.entity, where)
+    return { data, total }
   }
 }
 module.exports = Service
