a3c3d21c2be06850132fa3a17675ce5b6f7aa054
taizhenhui <52440231@qq.com>
2023-03-01 13:49:33 +0800

    小方块类

---
+++ typescript/Tetris/src/core/types.ts
@@ -0,0 +1,17 @@
+
+export interface IPoint {
+  readonly x: number;
+  readonly y: number;
+}
+
+export interface IViewer {
+  /**
+   * 显示
+   */
+  show():void;
+
+  /**
+   * 移除，不再显示
+   */
+  remove():void;
+}
--- typescript/Tetris/src/index.ts
+++ typescript/Tetris/src/index.ts
@@ -1,5 +1,26 @@
-let hello:string = 'world'
-console.log(hello);
+import { Square } from "./core/Square"
+import { IViewer } from "./core/types"
 
-export {}
+class SquareConsoleViewer implements IViewer {
+  constructor(private square: Square) {
+
+  }
+  show(): void {
+    console.log(this.square.point, this.square.color);
+
+  }
+
+  remove(): void {
+
+  }
+}
+const sq = new Square()
+
+sq.viewer = new SquareConsoleViewer(sq)
+sq.viewer.show()
+sq.point = {
+  x:5,
+  y:6
+}
+export { }
 
