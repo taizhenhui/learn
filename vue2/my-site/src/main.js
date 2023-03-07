50003475b98e65a4456513bda6204199e46882f3
taizhenhui <52440231@qq.com>
2023-03-07 15:30:08 +0800

    showMessage

--- vue2/my-site/src/main.js
+++ vue2/my-site/src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/global.less'
+import { showMessage } from './utils';
 
-
+Vue.prototype.$showMessage = showMessage
 new Vue({
   router,
   render: h => h(App),
--- vue2/my-site/src/views/Home/index.vue
+++ vue2/my-site/src/views/Home/index.vue
@@ -1,32 +1,35 @@
 <template>
-  <div class="home-container">
-    首页
+  <div class="home-container" ref="container">
+    <h1>首页</h1>
+    <button @click="handleClick">Click</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  components: {
-
-  },
-  data() {
-    return {
-
-    }
-  },
-  created () {
-
-  },
-  mounted () {
-
-  },
   methods: {
-
+    handleClick() {
+      const container = this.$refs.container
+      this.$showMessage({
+        content:'hello world',
+        type:'success',
+        duration: 1000,
+        container,
+        callback: ()=>{
+          console.log('h w');
+        }
+      })
+    },
   },
-}
+};
 </script>
 
-<style scoped lang="less">
-
+<style scoped>
+.home-container {
+  background: lightblue;
+  width: 300px;
+  height: 500px;
+  border: 1px solid;
+  margin: 50px auto;
+}
 </style>
