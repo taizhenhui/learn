9ac214fa0d9f1c0e54188d092ed609a9f8702081
taizhenhui <52440231@qq.com>
2023-03-09 14:29:12 +0800

    递归组件

---
+++ vue2/my-site/src/views/Blog/components/RightList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <ul class="right-list-container">
+      <li v-for="item in list" :key="item.name">
+        <span :class="{ active: item.isSelect }" @click="handleClick(item)">{{ item.name }}</span>
+        <RightList :list="item.children"  @click="handleClick(item)"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RightList',
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    handleClick(item) {
+      this.$emit('select', item)
+    },
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+@import "~@/styles/var.less";
+
+.right-list-container {
+  list-style: none;
+  padding: 0;
+
+  .right-list-container {
+    margin-left: 1em;
+  }
+
+  li {
+    min-height: 40px;
+    line-height: 40px;
+    cursor: pointer;
+
+    .active {
+      color: @warn;
+      font-weight: bold;
+    }
+  }
+}</style>
--- vue2/my-site/src/views/Blog/index.vue
+++ vue2/my-site/src/views/Blog/index.vue
@@ -1,30 +1,56 @@
 <template>
   <div class="blog-container">
+    <RightList :list="list" @select="handleSelect"/>
   </div>
 </template>
 
 <script>
+import RightList from './components/RightList.vue'
+import fetchData from '@/mixins/fetchData'
+import {getBlogTypes,getBlogs} from '@/api/blog'
 export default {
   name: 'blog',
+  mixins:[fetchData([])],
   components: {
+    RightList
   },
   data() {
     return {
-
+      list: [
+        { name: 'A', isSelect: false },
+        { name: 'B', isSelect: false },
+        {
+          name: 'C', isSelect: true, children: [
+            { name: 'c-1', isSelect: false },
+            {
+              name: 'c-2', isSelect: false, children: [
+                { name: 'c-2-1', isSelect: false },
+                { name: 'c-2-2', isSelect: false },
+              ]
+            },
+            { name: 'c-3', isSelect: false },
+            { name: 'c-4', isSelect: false },
+          ]
+        },
+        { name: 'D', isSelect: false },
+      ],
+      dataTypes:[],
+      dataBlogs:{}
     }
   },
-  created () {
 
-  },
-  mounted () {
-
-  },
   methods: {
-
+    async fetchData(){
+      this.dataTypes =  await getBlogTypes()
+      this.dataBlogs = await getBlogs()
+      console.log('this.dataTypes',this.dataTypes);
+      console.log('this.dataBlogs',this.dataBlogs);
+    },
+    handleSelect(item){
+      // console.log(item);
+    }
   },
 }
 </script>
 
-<style scoped lang="less">
-
-</style>
+<style scoped lang="less"></style>
