b28e271a70daee0d32233d50efc0c4f142620fda
DESKTOP-JVVML59\TAI <52440231@qq.com>
2023-03-20 20:47:47 +0800

    完成回显功能

--- calendar/index.js
+++ calendar/index.js
@@ -1,5 +1,4 @@
 $(function () {
-
   let minTime = moment().format('YYYY-MM-DD'), // 当前时间
     maxTime = moment().add(1, 'year').add(-1, 'd').format('YYYY-MM-DD'), // 最后时间
     minYear = moment().format('YYYY'), // 年
@@ -10,8 +9,9 @@ $(function () {
     selectYears = [],
     selectMonthsOne = [],
     selectMonthsTwo = [],
-    curCalendar = [], // 当前日历的数组
-    doms = {}
+    // curCalendar = [], // 当前日历的数组
+    doms = {}, // 获取的dom元素
+    selectedDates = [] // 已选择的日期
   /**
    * 初始化
    */
@@ -20,7 +20,8 @@ $(function () {
     doms = getDom()
 
     // 初始化 年
-    let minY = moment(minTime).format('YYYY'), maxY = moment(maxTime).format('YYYY');
+    let minY = moment(minTime).format('YYYY'),
+      maxY = moment(maxTime).format('YYYY')
     selectYears = minY === maxY ? [minY] : [minY, maxY]
 
     // 初始化 月
@@ -34,18 +35,14 @@ $(function () {
     }
 
     // 获取日历数组 初始化
-    curCalendar = createDatas(minYear, minMonth)
-    // 创建dom 初始化
+    createDatas(minYear, minMonth)
 
-
-    // 初始化 年月的下拉列表 
+    // 初始化 年月的下拉列表
     const { setYearDom, setMonthDom } = getDom()
     createOpt(setYearDom, selectYears)
     createOpt(setMonthDom, selectMonthsOne)
 
-
-
-    // 
+    //
     bindEvent()
   }
   init()
@@ -58,44 +55,47 @@ $(function () {
     nextDom.on('click', handleNext)
     setYearDom.on('change', handleSelectYear)
     setMonthDom.on('change', handleSelectMonth)
-    tabBody.delegate( '.tab-td', 'click', handleSelectDate)
-
+    tabBody.delegate('.tab-td', 'click', handleSelectDate)
   }
   // 绑定事件end
 
   // 事件处理start
   /**
-   * 设置新的 当前年月 
+   * 设置新的 当前年月
    */
   function setNewCurDate(date) {
     const { setMonthDom, setYearDom } = doms
     curYear = moment(date).format('YYYY')
     curMonth = moment(date).format('MM')
     setYearDom.val(curYear)
-    if (curYear === selectYears[1]) createOpt(setMonthDom, selectMonthsTwo)
+    if (curYear === selectYears[1]) {
+      createOpt(setMonthDom, selectMonthsTwo)
+    } else {
+      createOpt(setMonthDom, selectMonthsOne)
+    }
+
     setMonthDom.val(curMonth)
     createDatas(curYear, curMonth)
   }
 
   /**
-   * 处理上月 
+   * 处理上月
    */
   function handlePrev() {
     let curDate = curYear + '-' + curMonth
-    let minDate = moment(minTime).format('YYYY-MM');
+    let minDate = moment(minTime).format('YYYY-MM')
     if (curDate > minDate) {
       let newDate = moment(curDate).add(-1, 'months').format('YYYY-MM')
       setNewCurDate(newDate)
     }
   }
 
-
   /**
-   * 处理下月 
+   * 处理下月
    */
   function handleNext() {
     let curDate = curYear + '-' + curMonth
-    let maxDate = moment(maxTime).format('YYYY-MM');
+    let maxDate = moment(maxTime).format('YYYY-MM')
     if (curDate < maxDate) {
       let newDate = moment(curDate).add(1, 'months').format('YYYY-MM')
       setNewCurDate(newDate)
@@ -103,7 +103,7 @@ $(function () {
   }
 
   /**
-   * 处理选择年 
+   * 处理选择年
    */
   function handleSelectYear(e) {
     const { setMonthDom } = doms
@@ -120,31 +120,30 @@ $(function () {
   }
 
   /**
-   * 处理选择月 
+   * 处理选择月
    */
   function handleSelectMonth(e) {
     curMonth = e.target.value
     createDatas(curYear, curMonth)
   }
 
-  function handleSelectDate(){
-    // console.log($(this).attr('options'),'this');
+  function handleSelectDate() {
     let opts = JSON.parse($(this).attr('options'))
-    let classNames = $(this).prop('class') 
-    if(opts.isSelected === 'no') {
+    if (opts.isSelected === 'no') {
       opts.isSelected = 'yes'
-      $(this).prop('class', classNames + ' active')
-    }else{
+      $(this).addClass('active')
+      selectedDates.push(opts)
+    } else {
       opts.isSelected = 'no'
-      $(this).removeClass('active') 
+      $(this).removeClass('active')
+      selectedDates = selectedDates.filter((it) => it.date !== opts.date)
     }
     $(this).attr('options', JSON.stringify(opts))
-    
+    console.log(selectedDates, 'selectedDates')
   }
   // 事件处理end
 
-
-  /** 
+  /**
    * 创建 select的option
    */
   function createOpt(dom, arr) {
@@ -161,11 +160,10 @@ $(function () {
    * 生成当前月份数据
    * @param {String} y 年
    * @param {String} m 月
-   * @returns {Array} 二维数组 [[],[]] 
+   * @returns {Array} 二维数组 [[],[]]
    */
   function createDatas(y, m) {
     let time = y + '-' + m,
-
       curMonthArr = [],
       // 获取当月1号为星期几
       firstDayIsWeek = moment(time).date(1).weekday(),
@@ -173,13 +171,13 @@ $(function () {
       prevMonthDays = moment(time).subtract(1, 'month').daysInMonth(),
       // 获取当月天数
       curMonthDays = moment(time).daysInMonth(),
-
       // 获取第一排天数的数组
       firstArr = [],
-
-      prevMonth = moment(time).subtract(1, 'month').format("YYYY-MM"),
-      nextMonth = moment(time).month(moment(time).month() + 1).format("YYYY-MM"),
-      curMonth = moment(time).format("YYYY-MM");
+      prevMonth = moment(time).subtract(1, 'month').format('YYYY-MM'),
+      nextMonth = moment(time)
+        .month(moment(time).month() + 1)
+        .format('YYYY-MM'),
+      curMonth = moment(time).format('YYYY-MM')
 
     // 为第一排补充上月的天数
     for (let i = firstDayIsWeek - 1; i >= 0; i--) {
@@ -199,7 +197,11 @@ $(function () {
 
     // 获取剩余 天数的数组
     let arr = []
-    for (let o = firstArr[firstArr.length - 1].day + 1; o <= curMonthDays; o++) {
+    for (
+      let o = firstArr[firstArr.length - 1].day + 1;
+      o <= curMonthDays;
+      o++
+    ) {
       let date = curMonth + '-' + o.toString().padStart(2, '0')
       arr.push(createObjDate(date, true))
       // 中间排的数组
@@ -218,7 +220,7 @@ $(function () {
         arr = []
       }
     }
-    console.log('curMonthArr', curMonthArr);
+    // console.log('curMonthArr', curMonthArr);
     createDom(curMonthArr)
     return curMonthArr
   }
@@ -228,7 +230,7 @@ $(function () {
    * @param {Array}
    */
   function createDom(curCalendar) {
-    const {tabBody} = doms
+    const { tabBody } = doms
     tabBody.children().remove()
     for (const tr of curCalendar) {
       let trDom = $('<div></div>')
@@ -237,24 +239,22 @@ $(function () {
         tdDom.text(td.day)
         tdDom.prop('class', 'tab-td')
         tdDom.attr('options', JSON.stringify(td))
-        if (td.isDisabled) {
-          let classNames = tdDom.prop('class') + ' disabled'
-          tdDom.prop('class', classNames)
-        }
-        if (!td.isCurMonthDays) {
-          let classNames = tdDom.prop('class') + ' disCurMonthDays'
-          tdDom.prop('class', classNames)
-        }
-        trDom.prop('class', 'tab-tr')
+        // 给禁用的日期 添加类样式
+        if (td.isDisabled) tdDom.addClass("disabled")
+        // 给选中的日期 添加类样式
+        if (td.isSelected === 'yes')  tdDom.addClass("active")
+        // 给不是当月日期 添加类样式
+        if (!td.isCurMonthDays) tdDom.addClass("disCurMonthDays")
+
+        trDom.addClass('tab-tr')
         trDom.append(tdDom)
       }
       tabBody.append(trDom)
     }
-
   }
   /**
    * 获取元素
-   * @returns {Object} doms  
+   * @returns {Object} doms
    * prevDom（上月） nextDom（下月） setYearDom（设置年） setMonthDom（设置月）
    * tabBody（日历） trDom(tr) tdDom(td)
    */
@@ -273,27 +273,34 @@ $(function () {
     }
   }
   /**
-   * 创建日期对象格式 
+   * 创建日期对象格式
    * @param {String} date 时间 年月日
    * @param {Boolean} isCurMonthDays 是否是点前月的天数 true 是 false 否
    * @returns {Object} obj 时间对象
    */
   function createObjDate(date, isCurMonthDays) {
     let obj = {
-      isSelected: 'no', // 是否被选中 yes 选中，no 未选中
+      isSelected: isSelectedDom(date) ? 'yes' : 'no', // 是否被选中 yes 选中，no 未选中
       isDisabled: isDisableDate(date), // 是否禁用该时间 true 禁用（不可选），false 不禁用（可选）
       isCurMonthDays, // 是否是当前月的日期
-      year: parseFloat(moment(date).format("YYYY")), // 年
-      mouth: parseFloat(moment(date).format("MM")), // 月
-      day: parseFloat(moment(date).format("DD")), // 日
-      date: moment(date).format("YYYY-MM-DD"), // 年 - 月 - 日 时间
+      year: parseFloat(moment(date).format('YYYY')), // 年
+      mouth: parseFloat(moment(date).format('MM')), // 月
+      day: parseFloat(moment(date).format('DD')), // 日
+      date: moment(date).format('YYYY-MM-DD'), // 年 - 月 - 日 时间
     }
-
 
     return obj
   }
   /**
-   * 判断该时间是否禁用 
+   * 判断该时间是否被选中
+   * @param {String} date 时间 年月日
+   * @returns {Boolean} true：被选中 false：未选中
+   */
+  function isSelectedDom(date) {
+    return selectedDates.some((s) => s.date === date)
+  }
+  /**
+   * 判断该时间是否禁用
    * @param {String} date 时间 年月日
    * @returns {Boolean} true 禁用 false 不禁用
    */
@@ -307,5 +314,4 @@ $(function () {
     }
     return false
   }
-
 })
