10a48147c67b00a44620a3f17fbdea64ce33b500
DESKTOP-JVVML59\TAI <52440231@qq.com>
2023-03-24 21:58:12 +0800

    小程序 搭建

--- wxapp/rellux-rights-wxapp/app.js
+++ wxapp/rellux-rights-wxapp/app.js
@@ -7,16 +7,9 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-    wx.getSystemInfo({
-      success: res => {
-        console.log('res',res);
-      this.globalData.height = res.statusBarHeight
-      }
-     })
   },
 
   globalData: {
     userInfo: null,
-    height: 0,
   }
 })
