ca80db13f04a9d8a537386298a1f0fcd40da6de3
taizhenhui <52440231@qq.com>
2023-03-27 14:52:45 +0800

    项目

--- vue2/my-site/src/store/index.js
+++ vue2/my-site/src/store/index.js
@@ -3,13 +3,15 @@ import Vuex from 'vuex'
 import banner from './banner'
 import setting from './setting'
 import about from "./about";
+import project from "./project";
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     banner,
     setting,
-    about
+    about,
+    project
   },
   strict: true,   // 开启严格模式后，只允许通过mutation改变
 })
