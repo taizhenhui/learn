2ed7409bf3ea2d72c0c2327ff1f585398c9ffd3a
taizhenhui <52440231@qq.com>
2023-04-23 13:00:12 +0800

    修改sidebar样式

--- egg/rellux-app/src/views/layout/AsideBar.vue
+++ egg/rellux-app/src/views/layout/AsideBar.vue
@@ -1,19 +1,40 @@
 <template>
-  <div class="aside-bar">
-    <div class="nav-sty">
-      <h1>导航菜单</h1>
-      <Icon class="icon-sty" type="close" />
+  <div
+    class="aside-bar"
+    :style="{ width: isCollapse ? '64px' : '250px', transition: '0.5s' }"
+  >
+    <div
+      class="nav-sty"
+      :style="{ justifyContent: isCollapse ? 'flex-start' : 'space-between' }"
+    >
+      <h1
+        :style="{
+          opacity: isCollapse ? 0 : 1,
+          overflow: 'hidden',
+          width: isCollapse ? '0' : '',
+          transition: '0.3s',
+        }"
+      >
+        导航菜单
+      </h1>
+      <Icon
+        class="icon-sty"
+        :style="{ transform: isCollapse ? ' rotate(180deg)' : '', transition: '0.3s' }"
+        type="close"
+        @click="handleCollapse"
+      />
     </div>
     <el-menu
       class="el-menu-vertical-demo"
       :collapse="isCollapse"
+      text-color="#333"
       @open="handleOpen"
-      @close="handleClose"
+      @close="handleOpen"
     >
       <el-sub-menu
         v-for="(item, index) in menuRoutes"
-        :index="(index + 1).toString()"
-        :class="{}"
+        :index="index.toString()"
+        :class="{ active: index === indexRef }"
         :key="item.name"
       >
         <template #title>
@@ -22,7 +43,7 @@
         </template>
         <el-menu-item
           v-for="(it, i) in item.children"
-          :index="index + 1 + '-' + (i + 1)"
+          :index="index + '-' + i"
           :key="it.name"
         >
           <span class="sub-title"></span> {{ it.meta?.title }}</el-menu-item
@@ -44,27 +65,28 @@ const getMenuRoutes = (routes: Array<RouteRecordRaw>) => {
 };
 const menuRoutes = getMenuRoutes(routes);
 
-const isCollapse = ref(false);
-const indexRef = ref("");
-const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
-  indexRef.value = key;
+const isCollapse = ref<boolean>(false);
+const indexRef = ref<number>(-1);
+const handleOpen = (key: string) => {
+  indexRef.value = +key;
 };
-const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
+
+const handleCollapse = () => {
+  isCollapse.value = !isCollapse.value;
 };
 </script>
 
 <style scoped lang="less">
 @import "~@/styles/mixin.less";
+@color: #625df7;
 .aside-bar {
-  width: 250px;
+  // width: 250px;
   height: 100vh;
   background-color: #fff;
   color: #333333;
   .nav-sty {
     width: 100%;
-    height: 56px;
+    height: 50px;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -81,50 +103,44 @@ const handleClose = (key: string, keyPath: string[]) => {
       color: #9f9f9f;
     }
   }
-  .el-menu {
-    border-right: none;
-    overflow: scroll;
-  }
-  .el-menu-vertical-demo {
-    height: 100%;
-  }
-
-  ::v-deep .el-menu-item {
-    height: 48px;
-
-    .sub-title {
-      display: block;
-      width: 10px;
-    }
-  }
-  ::v-deep .is-active {
-    background-color: #e2e0f6;
-    color: #625df7;
-  }
-  ::v-deep .el-sub-menu__title {
-    height: 48px;
-  }
-  ::v-deep(.el-sub-menu__title:hover) {
-    background-color: #625df7 !important;
-    color: #fff;
-  }
+}
+.el-menu {
+  border-right: none;
+  overflow: scroll;
+}
+.el-menu-vertical-demo {
+  height: 100%;
 }
 
-// .el-menu-vertical-demo:not(.el-menu--collapse) {
-//   width: 250px;
-// }
-// .title_sty {
-//   height: 60px;
-// }
 
-// .open {
-//   width: 200px;
-//   transition: 0.4s;
-//   background-color: #545c64;
-// }
-// .close {
-//   width: 64px;
-//   transition: 0.4s;
-//   background-color: #545c64;
-// }
+::v-deep .el-sub-menu__title {
+  height: 42px;
+}
+::v-deep .el-sub-menu__title:hover {
+  background-color: @color !important;
+  color: #fff !important;
+}
+
+.el-menu-item {
+  height: 42px;
+  transition: 0.1s;
+}
+.el-menu-item:hover {
+  background-color: #fff;
+  color: @color;
+  transition: 0.1s;
+}
+.el-menu-item.is-active {
+  background-color: #e2e0f6 !important;
+  border-left: 3px solid @color;
+  color: @color !important;
+}
+.active {
+  &.el-sub-menu {
+    background-color: @color !important;
+    ::v-deep .el-sub-menu__title {
+      color: #fff !important;
+    }
+  }
+}
 </style>
