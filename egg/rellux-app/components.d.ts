8660d40e823a3ea0c3bbd43bb5b36c7c2d8ac6c8
taizhenhui <52440231@qq.com>
2023-04-24 10:48:14 +0800

    登录页面完成

--- egg/rellux-app/components.d.ts
+++ egg/rellux-app/components.d.ts
@@ -11,6 +11,7 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElInput: typeof import('element-plus/es')['ElInput']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
--- egg/rellux-app/src/views/login/index.vue
+++ egg/rellux-app/src/views/login/index.vue
@@ -9,12 +9,34 @@
           <img src="@/assets/login/login-draw.png" alt="" />
         </div>
       </div>
-      <div class="right"></div>
+      <div class="right">
+        <h1 class="title">欢迎登录</h1>
+        <div class="input-sty">
+          <el-input v-model="account" placeholder="请输入用户名" />
+          <el-input
+            v-model="password"
+            type="password"
+            placeholder="请输入您的密码"
+            show-password
+          />
+          <div class="code-box">
+            <el-input v-model="code" placeholder="请输入验证码" />
+            <img src="" alt="" />
+          </div>
+        </div>
+        <button class="login-btn">登录</button>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+const account = ref<string>("");
+const password = ref<string>("");
+const code = ref<string>("");
+
+</script>
 
 <style scoped lang="less">
 @import "~@/styles/mixin.less";
@@ -40,19 +62,52 @@
     overflow: hidden;
     .self-center();
     .flex-common();
-    .left, .right{
-     width: 50%;
+    .left,
+    .right {
+      width: 50%;
     }
-    .left{
+    .left {
       height: 100%;
-      background-color: #E2E0F6;
+      background-color: #e2e0f6;
       .flex-common(center, center);
-      .login-draw{  
+      .login-draw {
         width: 70%;
         margin: auto;
-        img{
+        img {
           width: 100%;
         }
+      }
+    }
+    .right {
+      // .flex-common(center, center);
+      // flex-direction: column;
+      
+      padding: 0 12%;
+      .title {
+        color: @words;
+        font-size: 24px;
+        font-weight: bold;
+        width: 100%;
+        margin-bottom: 30px;
+      }
+      .input-sty{
+        .el-input{
+          margin-bottom: 20px;
+          height: 38px;
+        }
+        :deep(.el-input__wrapper.is-focus){
+          box-shadow: 0 0 0 1px @theme inset;
+        }
+      }
+      .login-btn{
+        width: 100%;
+        height: 38px;
+        border: none;
+        line-height: 38px;
+        text-align: center;
+        border-radius: 30px;
+        background-color: @theme;
+        color: #fff;
       }
     }
   }
