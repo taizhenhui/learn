a71e3897cadd2073ae2d523762da93024314358d
taizhenhui <52440231@qq.com>
2023-11-21 17:56:07 +0800

    路由信息

--- react/react-learn/src/App.js
+++ react/react-learn/src/App.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import qs from 'query-string'
 
-import Login from './pages/Login'
-import Admin from './pages/Admin'
+function A(props) {
+    console.log(props);
+    const {location} = props;
+    const query = qs.parse(location.search);
+    const hash = qs.parse(location.hash);
+    console.log(query, hash);
+    return (
+        <>
+            <h1>A 组件</h1>
+            <p>访问地址： {props.location.pathname}</p>
+            <p>查询参数：a:{query.a} - b:{query.b} - c:{query.c}</p>
+            <button onClick={() => {
+                props.history.push('/b', '给b的状态数据')
+            }}>跳转到 B组件</button>
+        </>
+    )
+}
+function B(props) {
+    return (
+        <>
+            <h1>B 组件</h1>
+            <p>获取状态数据：{props.history.location.state}</p>
+            <button onClick={() => {
+                props.history.push('/a?a=1&b=2&c=3#qwer')
+            }}>跳转到 A组件</button>
+        </>
+    )
+}
 export default function App() {
+
     return (
         <Router>
             <Switch>
-                <Route path='/login' exact component={Login} />
-                <Route path='/' component={Admin} />
+                <Route path="/a" component={A}></Route>
+                <Route path="/b" component={B}></Route>
             </Switch>
         </Router>
     )
