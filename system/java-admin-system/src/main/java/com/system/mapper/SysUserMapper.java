a199d751cb345514e6baf0f9ea0a910d380d8fdd
taizhenhui <52440231@qq.com>
2024-01-08 13:10:17 +0800

    权限管理学习

---
+++ system/java-admin-system/src/main/java/com/system/mapper/SysUserMapper.java
@@ -0,0 +1,15 @@
+package com.system.mapper;
+
+import com.system.entity.SysUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Entity com.system.entity.SysUser
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+}
+
+
+
+
---
+++ system/java-admin-system/src/main/java/com/system/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.system.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.system.entity.SysUser;
+import com.system.service.SysUserService;
+import com.system.mapper.SysUserMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author JS04
+ * @description 针对表【sys_user】的数据库操作Service实现
+ * @createDate 2024-01-08 12:38:23
+ */
+@Service
+public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements SysUserService {
+
+}
+
+
+
+
